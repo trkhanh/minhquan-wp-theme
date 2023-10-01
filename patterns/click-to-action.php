b7bdcaa27465eb3b9db12d35c8545981b1979745
trkhanh <tran_khanh@outlook.com>
2023-10-01 14:20:08 +0700

    feat: implement CTA and Video

--- patterns/click-to-action.php
+++ patterns/click-to-action.php
@@ -1,9 +1,64 @@
 <?php
 /**
  * Title: Click to action
- * Slug: minhquantemplate/cta
+ * Slug: minhquantemplate/clicktoaction
  * Categories: featured
- * Keywords: Call to action
+ * Keywords: Click to action
  * Block Types: core/buttons
  */
 ?>
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide" style="flex-direction: column-reverse">
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"x-large"} -->
+		<p class="has-large-font-size" style="line-height:1.2">
+            <span class="wp-block-text__primary">
+			Tôm giống minh quân
+            </span> 
+		</p>
+		<!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"style":{"typography":{"lineHeight":"0"}},"fontSize":"x-large"} -->
+		<p class="has-large-font-size" style="line-height:0">
+            <span class="wp-block-text__secondary">
+                Luôn dẫn đầu về chất lượng
+            </span>
+		</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
+</div>
+
+<div class="wp-block-columns alignwide" style="flex-direction: column-reverse">
+	<!-- wp:column -->
+	<div class="wp-block-column">
+		<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.2"}},"fontSize":"x-large"} -->
+		<p class="has-large-font-size" style="line-height:1.2">
+            <span class="wp-block-text__secondary">
+			Áp dụng công nghệ thủy sản tiên tiến
+            </span> <span class="wp-block-text__primary">trong phát triên con giống tôm càng xanh</span>
+		</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons -->
+		<div class="wp-block-buttons">
+			<!-- wp:button {"fontSize":"small"} -->
+			<div class="wp-block-button has-custom-font-size has-small-font-size">
+				<a class="wp-block-button__link wp-element-button">
+                Liên Hệ Ngay
+				</a>
+			</div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+	<!-- /wp:column -->
+
+	<div class="wp-block-column">
+		<!-- wp:separator {"className":"is-style-wide"} -->
+		<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+		<!-- /wp:separator -->
+	</div>
+</div>
+<!-- /wp:columns -->
